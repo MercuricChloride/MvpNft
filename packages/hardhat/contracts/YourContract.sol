b3dfc0aa20db140e32cef6a1d7cbda4b1d0bcd79
mercury-protocol <84585728+mercury-protocol@users.noreply.github.com>
2021-07-13 11:46:54 -0400

    Functionality is now working. Still confused about the UI bug.
    The NFT is now functional. However the safeTransferFrom functions still aren't working in the UI. So for now just test with the safeTransferFromShouldWorkNow function... not sure why this is.

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 //import "hardhat/console.sol";
-//import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+
 interface ERC721 {
     event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
     event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId);
@@ -37,17 +37,18 @@ contract YourContract is ERC721, ERC165 {
     uint public tokenId;
     mapping (uint => address) public approvedList;
     mapping (address => mapping (address => bool)) public operatorList;
-
-//    event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
-//    event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId);
-//    event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
-
     //currently declaring the owner as my local acct on scaffold-eth. Will be msg.sender later
     constructor(string memory _arbString) {
         arbString = _arbString;
         owner = 0xC9FFEe9e34723d882CB97a6c056100661d00Bfe1;
         tokenId = 1;
         emit Transfer(address(0), address(0), tokenId);
+    }
+
+    function isContract(address addr) public view returns(bool) {
+      uint size;
+      assembly { size := extcodesize(addr) }
+      return size > 0;
     }
 
     function balanceOf(address queryAddress) external view override returns (uint) {
@@ -66,20 +67,56 @@ contract YourContract is ERC721, ERC165 {
         }
     }
 
+    function safeTransferFromShouldWorkNow(address _from, address _to, uint _tokenId) external payable {
+        require(msg.sender == owner || operatorList[owner][msg.sender] == true || approvedList[tokenId] == msg.sender);
+        require(_from == owner && _from != address(0) && _tokenId == tokenId);
+        if(isContract(_to)) {
+            if(ERC721TokenReceiver(_to).onERC721Received(msg.sender, _from, _tokenId, "") == 0x150b7a02) {
+                emit Transfer(_from, _to, _tokenId);
+                owner = _to;
+            }
+            else {
+                revert("receiving address unable to hold ERC721!");
+            }
+        }
+        else {
+            emit Transfer(_from, _to, _tokenId);
+            owner = _to;
+        }
+    }
+
     function safeTransferFrom(address _from, address _to, uint _tokenId, bytes memory data) external override payable {
         require(msg.sender == owner || operatorList[owner][msg.sender] == true || approvedList[tokenId] == msg.sender);
         require(_from == owner && _from != address(0) && _tokenId == tokenId);
-        ERC721TokenReceiver(_to).onERC721Received(_from, _from, _tokenId, data);
-        emit Transfer(_from, _to, _tokenId);
-        owner = _to;
+        if(isContract(_to)) {
+            if(ERC721TokenReceiver(_to).onERC721Received(msg.sender, _from, _tokenId, data) == 0x150b7a02) {
+                emit Transfer(_from, _to, _tokenId);
+                owner = _to;
+            } else {
+                revert("receiving address unable to hold ERC721!");
+            }
+        } else {
+            emit Transfer(_from, _to, _tokenId);
+            owner = _to;
+        }
     }
 
     function safeTransferFrom(address _from, address _to, uint _tokenId) external override payable {
         require(msg.sender == owner || operatorList[owner][msg.sender] == true || approvedList[tokenId] == msg.sender);
         require(_from == owner && _from != address(0) && _tokenId == tokenId);
-        ERC721TokenReceiver(_to).onERC721Received(_from, _from, _tokenId, "");
-        emit Transfer(_from, _to, _tokenId);
-        owner = _to;
+        if(isContract(_to)) {
+            if(ERC721TokenReceiver(_to).onERC721Received(msg.sender, _from, _tokenId, "") == 0x150b7a02) {
+                emit Transfer(_from, _to, _tokenId);
+                owner = _to;
+            }
+            else {
+                revert("receiving address unable to hold ERC721!");
+            }
+        }
+        else {
+            emit Transfer(_from, _to, _tokenId);
+            owner = _to;
+        }
     }
 
     function transferFrom(address _from, address _to, uint _tokenId) external override payable {
